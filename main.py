72f24e77af0b5b48475b895c0d54c95d2d38d451
Georges Hattab <ghattab@users.noreply.github.com>
2017-02-10 11:46:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-## ./main.py -i ../data/pics_kolonie_4/ -p
+## Usage: ./main.py -i ../data/pics_kolonie_4/ -p
 
 # ------------------------------------------------------------------------------
 #                                  MODULES IMPORT
